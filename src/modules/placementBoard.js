cefb8bb3718d6b3d9073ff2129551fa79cca9b08
Niall Craven <88824327+niallantony@users.noreply.github.com>
2023-08-23 23:48:30 +0900

    Add illegal tile highlighting to drag and drop placement system

--- src/modules/placementBoard.js
+++ src/modules/placementBoard.js
@@ -3,6 +3,9 @@ import { Ship } from "./ship.js"
 import { SHIP_IMAGES } from "./screen.js"
 
 export const PlacementBoard = (gameboard) => {
+    let placing = false;
+    const shipBar = document.getElementById('ship-bar');
+
     const ships = {
         carrier:{
             coords:[],
@@ -60,7 +63,7 @@ export const PlacementBoard = (gameboard) => {
             for (let j = 0 ; j < size ; j++ ) {
                 const tile = document.createElement('button');
                 tile.classList.add('tile');
-                tile.id = String(j)+i;
+                tile.id = `${j}-${i}`;
                 tile.setAttribute('style','position:relative;')
                 tile.classList.add(gameboard.squareStatus(j,i));
                 rowContainer.appendChild(tile);
@@ -70,34 +73,45 @@ export const PlacementBoard = (gameboard) => {
 
     const renderPlacementScreen = () => {
         drawPlacementBoard();
-        const shipBar = document.getElementById('ship-bar');
-        const shipsNames = Object.keys(ships);
-        shipsNames.forEach((ship) => {
-            const buttonText = String('Place '+ship).toUpperCase();
+        drawNextShipButton();
+    }
+
+    const drawNextShipButton = () => {
+        const button = makeNextShipButton();
+        button.addEventListener('click',() => {
+            shipBar.removeChild(button);
+            const ship = makeShip(button);
+            shipPlacement(ship,ships[ship]);
+        });
+        shipBar.appendChild(button);
+    }
+
+    const clearShipBar = () => {
+        const existing = document.querySelector('.place-ship');
+        if (existing) existing.parentNode.removeChild(existing);
+    }
+
+    const makeNextShipButton = () => {
+        clearShipBar();
+        for (let key in ships) {
+            if (ships[key].placed) continue;
+            const buttonText = String('Place '+ key).toUpperCase();
             const button = document.createElement('button');
             button.classList.add('place-ship');
-            button.id = ship;
+            button.id = key;
             button.textContent = buttonText;
-            shipBar.appendChild(button);
-            button.addEventListener('click',() => {
-                shipBar.removeChild(button);
-                const ship = makeShip(button);
-                shipPlacement(ship,ships[ship]);
-            })
-        });
+            return button;
+        }   
+        return renderSubmitButton();
     }
 
     const makeShip = (button) => {
         const ship = Ship(button.id);
         ship.rotate();
-        console.log(findIllegalSquares(ship));
         return ship
     }
 
-
-
-    const shipPlacement = (ship) => {
-        const tiles = document.querySelectorAll('.tile');
+    const createTemplate = (ship) => {
         const template = document.createElement('button');
         template.classList.add('placeholder');
         template.id = ship.name;
@@ -105,13 +119,45 @@ export const PlacementBoard = (gameboard) => {
         template.style.top = '0px';
         template.style.left = '0px';
         template.style.backgroundImage = `url(${SHIP_IMAGES[ship.name]}`;
+        return template;
+    }
+
+    const clearRotateButton = () => {
+        shipBar.querySelectorAll('.rotate').forEach((button) => shipBar.removeChild(button));
+    }
+
+    const createRotateButton = () => {
+        clearRotateButton();
+        const button = document.createElement('button');
+        button.classList.add('rotate');
+        button.textContent = 'Rotate';
+        shipBar.appendChild(button);
+        return button;
+    }
+
+
+    const shipPlacement = (ship) => {
+        placing = true;
+        const tiles = document.querySelectorAll('.tile');
+        const template = createTemplate(ship);
         const board = document.getElementById('left');
         board.appendChild(template);
         renderShip(template,tiles[0].offsetWidth,ship);
-        console.log(ship.orientation);
+        const illegalSquares = findIllegalSquares(ship);
+        TEMPconsoleIllegalTiles(illegalSquares);
+        const rotate = createRotateButton();
+        rotate.addEventListener('click',() => {
+            removeMarker(template);
+            rotateShip(ship);
+        })
         tiles.forEach((tile) => {
-            if (isOutOfBounds(ship.orientation,ship.length,tile)) return;
             hoverImage(tile,template);
+            if (illegalSquares.includes(tile.id)) {
+                tile.classList.add('illegal');
+                return;
+            } else {
+                tile.classList.remove('illegal');
+            }
             tile.addEventListener('click',(e) => {
                 placeTemplate(e.target.closest('.tile'),template,ship);
             });
@@ -124,21 +170,38 @@ export const PlacementBoard = (gameboard) => {
         for ( let i = 0 ; i < gameboard.getLength() ; i++ ) {
             for ( let j = gameboard.getLength() - (ship.length - 1); j < gameboard.getLength() ; j++ ) {
                 const oobMove = ship.orientation ? [j,i] : [i,j] ;
-                illegalSquares.push(oobMove);
+                illegalSquares.push(oobMove.join('-'));
             }
         }
-        return illegalSquares ;
+        //Get spaces that are obstructed by another ship
+        for (let key in ships) {
+            const spaceSet = new Set();
+            if (!ships[key].placed) continue;
+            const illMoves = getOccupiedSpaces(ships[key]);
+            const arrayPointer = ship.orientation ? 0 : 1; 
+            illMoves.forEach((space) => {
+                spaceSet.add(space.join('-'));
+                for( let i = 0 ; i < ship.length ; i++ ) {
+                    const nextSpace = [...space];
+                    nextSpace[arrayPointer] = nextSpace[arrayPointer] - i;
+                    if (nextSpace[arrayPointer] < 0) continue ; 
+                    spaceSet.add(nextSpace.join('-'));
+                }
+            });
+            spaceSet.forEach((coord) => illegalSquares.push(coord));
+        }
+        return illegalSquares;
     }
 
-    const isOutOfBounds = (orientation,length, tile) => {
-        const tileLength = tile.parentNode.children.length;
-        if (orientation) {
-            if ((length + Number(tile.id.split('')[0])) > tileLength) return true;
-            return false;
-        } else {
-            if ((length + Number(tile.id.split('')[1])) > tileLength) return true;
-            return false;
+    const getOccupiedSpaces = (marker) => {
+        const spaces = new Set();
+        const arrayPointer = marker.horizontal ? 0 : 1 ;
+        for ( let i = 0 ; i < marker.length ; i++ ) {
+            const currentCoord = [...marker.coords];
+            currentCoord[arrayPointer] = currentCoord[arrayPointer] + i;
+            spaces.add(currentCoord);
         }
+        return spaces;
     }
 
     const renderShip = (image,unit,ship) => {
@@ -148,29 +211,44 @@ export const PlacementBoard = (gameboard) => {
         image.style.height = height;
     }
 
-    const moveShip = (template,ship) => {
+    const removeMarker = (template) => {
         template.parentNode.removeChild(template);
+        const tiles = document.querySelectorAll('.tile');
+        tiles.forEach((tile) => {
+            tile.replaceWith(tile.cloneNode(true));
+        })
+    }
+
+    const rotateShip = (ship) => {
+        ship.rotate();
+        shipPlacement(ship);
+    }
+
+    const moveShip = (template,ship) => {
+        if (placing) return;
+        clearShipBar();
+        template.parentNode.removeChild(template);
+        ships[ship.name].placed = false;
         shipPlacement(ship);
     }
 
     const placeTemplate = (tile,template,ship) => {
+        clearRotateButton();
         const coords = Screen.getTarget(tile);
         const position = calculateTemplatePosition(tile.offsetWidth,coords);
         template.style.top = position.top;
         template.style.left = position.left;
         template.style.zIndex = 10;
         ships[template.id].coords = coords;
+        ships[template.id].horizontal = ship.orientation;
         ships[template.id].placed = true;
         template.addEventListener('click',(e) => moveShip(e.target.closest('.placeholder'),ship));
         const tiles = document.querySelectorAll('.tile');
         tiles.forEach((tile) => {
             tile.replaceWith(tile.cloneNode(true));
         })
-        if (!checkForUnplaced()) {
-            renderSubmitButton();
-        } else {
-            removeSubmitButton();
-        }
+        placing = false;
+        drawNextShipButton();
     }
 
     const calculateTemplatePosition = (unit,coords) => {
@@ -183,21 +261,37 @@ export const PlacementBoard = (gameboard) => {
     }
 
     const renderSubmitButton = () => {
-        const shipBar = document.getElementById('ship-bar');
         const submitButton = document.createElement('button');
         submitButton.classList.add('submit-placement');
-        shipBar.appendChild(submitButton);
+        submitButton.textContent = 'Submit';
+        return submitButton
     }
 
-    const removeSubmitButton = () => {
-        const submitButton = document.querySelectorAll('.submit-placement');
-        submitButton.forEach((button) => button.parentNode.removeChild(button));
+    const TEMPconsoleIllegalTiles = (illegal) => {
+        const consoleString = ['']
+        for (let i = 0 ; i < 10 ; i++) {
+            for (let j = 0 ; j < 10 ; j++ ) {
+                if (illegal.includes(`${j}-${i}`)) {
+                    consoleString.push('X');
+                } else {
+                    consoleString.push('O');
+                }
+            }
+            consoleString.push('\n');
+        }
+        console.log(consoleString.join(' '));
     }
 
     const hoverImage = (element,img) => {
         const event = element.addEventListener('mouseover',(e) => {
-            const coords = Screen.getTarget(e.target.closest('.tile'));
-            const pos = calculateTemplatePosition(e.target.closest('.tile').offsetWidth,coords);
+            const tile = e.target.closest('.tile');
+            const coords = Screen.getTarget(tile);
+            const pos = calculateTemplatePosition(tile.offsetWidth,coords);
+            if(tile.classList.contains('illegal')) {
+                img.classList.add('out-of-bounds');
+            } else {
+                img.classList.remove('out-of-bounds');
+            }
             img.style.top = pos.top;
             img.style.left = pos.left;
         })
